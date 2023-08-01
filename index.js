2cb71f3e6ed6f0a32a469c1cc66e3d40f3d44c11
Jazlin Yu <jazlinsub@gmail.com>
2023-07-31 17:44:29 -0700

    passing test

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+/** @format */
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+function gerateRandomIntegerGreaterThanZero() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+const random = gerateRandomIntegerGreaterThanZero();
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+const numbers = [1, 7, 20, 18];
+const max = Math.max(...numbers);
